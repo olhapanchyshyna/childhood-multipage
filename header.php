204f90c1b9c28aebc4c7aa602469f064571cda3c
Olya <olapancisina1@gmail.com>
2023-07-07 10:42:59 +0200

    add meta

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
         <meta name="theme-color" content="#c9e0e04d">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="title" content="Мир дества">
+        <meta name="description" content="Мы воплощаем все детские мечты и помогаем родителям дарить счастливое детство!">
         <link rel="shortcut icon" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/favicon.ico" type="image/x-icon">
         <link rel="apple-touch-icon" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon.png">
         <link rel="apple-touch-icon" sizes="57x57" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/apple-touch-icon-57x57.png">
